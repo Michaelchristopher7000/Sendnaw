01f8ed9597ca81b82e8799501d245eb73bbfd296
Michaelchristopher7000 <mikec9613@gmail.com>
2026-06-19 14:21:35 +0100

    fif

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://sendnawtechnologies.infinityfree.io',
+        target: 'https://sendnawtechnologies.infinityfree.io',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
