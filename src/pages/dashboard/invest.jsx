13a5a2fbcc224e14538f1ad0bb1baf04ab1fa759
Michaelchristopher7000 <mikec9613@gmail.com>
2026-06-19 17:48:25 +0100

    Fix useNavigate undefined in invest page

--- src/pages/dashboard/invest.jsx
+++ src/pages/dashboard/invest.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import { showNotification } from "../../utils/notify";
 import { useTheme } from "../../context/themecontext";
 
