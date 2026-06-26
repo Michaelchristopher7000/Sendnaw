fbc1b35ee769ce38e1c98fb89681dcef6f7e5de1
Michaelchristopher7000 <mikec9613@gmail.com>
2026-06-26 14:46:00 +0100

    new file convert.js

--- api/convert.js
+++ api/convert.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         return res.status(200).end();
     }
 
-    // 3. Only allow GET requests
+    // 3. Only allow GET requests.
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
